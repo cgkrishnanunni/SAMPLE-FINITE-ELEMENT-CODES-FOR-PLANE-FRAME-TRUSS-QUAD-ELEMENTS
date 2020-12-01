951e837c799cb24292ad97789b508fecae829788
cgkrishnanunni <67425535+cgkrishnanunni@users.noreply.github.com>
2020-12-02 03:36:20 +0530

    Create genQuad4.cpp

---
+++ Quad4/genQuad4.cpp
@@ -0,0 +1,151 @@
+// Mesh generator for a cantilever beam
+// Uses four-noded isoparametric quadrilateral elements
+
+#include <fstream>
+
+using namespace std;
+
+void main(void)
+{
+	int  nDivSpan, nDivDepth;
+	int nNodes, nElems, probType;
+	char file[10];
+	int i, j, k, elem, node;
+	double x[10000], y[10000];
+	int elemConn[5][10000];
+	double span, depth, thick, E, po, load, bx, by;
+
+	ifstream fin("genQuad4.inp");
+	ofstream fout("quad4.inp");
+	
+	fout.precision(20);
+	
+	fin >> file;
+	fin >> probType;
+	fin >> span >> depth >> thick >> nDivSpan >> nDivDepth;
+	fin >> E >> po;
+	fin >> load >> bx >> by;
+
+	int supType; // support and loading type
+	fin >> supType; // 1: uniform axial loading along x-dirn; 2: couple moment at end
+	double theta; // angle of distortion
+	fin >> theta;
+	theta = theta * 3.1415926/180.0;
+
+	nElems = nDivDepth*nDivSpan;
+	nNodes = (nDivDepth + 1)*(nDivSpan + 1);
+
+// Generating nodal coordinate data
+	node = 0;
+	double xx;
+	for (j=1; j<=nDivSpan + 1; ++j)
+	{
+		xx = span/nDivSpan*(j - 1);
+		for (i=1; i<=nDivDepth + 1; ++i)
+		{
+			node ++;
+			x[node] = xx;
+			y[node] = depth/nDivDepth*(i - 1);
+		}
+	}
+
+// Mesh Distortion:
+	node = nDivDepth + 1;
+	for (i=node + 1; i<=nNodes - node; i++)
+	{
+		if (i <= (nDivDepth + 1)*(nDivSpan/2))
+			x[i] = x[i] + theta*(y[i] - depth/2.0);
+		else if (i > (nDivDepth + 1)*(nDivSpan/2 + 1))
+			x[i] = x[i] - theta*(y[i] - depth/2.0);
+	}
+
+// Generate element connectivity
+
+	// First element
+	elemConn[1][1] = 1;
+	elemConn[2][1] = nDivDepth + 2;
+	elemConn[3][1] = elemConn[2][1] + 1;
+	elemConn[4][1] = 2;
+	
+	// First layer along left strip
+	elem = 1;
+	for (i=2; i<=nDivDepth; ++i)
+	{
+		elem ++;
+		for (j=1; j<=4; ++j)
+			elemConn[j][elem] = elemConn[j][elem-1] + 1;
+	}
+
+	// Subsequent layers
+	elem = nDivDepth;
+	for (i=2; i<=nDivSpan; ++i)
+	{
+		for (j=1; j<=nDivDepth; ++j)
+		{
+			elem ++;
+			for (k=1; k<=4; ++k)
+				elemConn[k][elem] = elemConn[k][elem-nDivDepth] + (nDivDepth + 1);
+		}
+	}
+
+	fout.precision(12);
+// Printing results
+	int nGauss = 2;
+	fout << file << "\n";
+	fout << nGauss << "   " << probType;
+	fout << "\n" << nNodes << "   " << nElems << "\n";
+
+// Printing nodal coordinates
+	for (i=1; i<=nNodes; ++i)
+		fout << "\n" << i << "   " << x[i] << "    " << y[i];
+
+	fout << "\n";
+//	Printing element Connectivity
+	for (i=1; i<=nElems; ++i)
+	{
+		fout << "\n"<< i << "  ";
+		for (j=1; j<=4; ++j)
+			fout << elemConn[j][i] << "  ";
+		fout << thick << "  " << E << "  " << po;
+	}
+
+// Displacement boundary condition data
+	fout << "\n\n" << nDivDepth + 1;
+	fout << "\n1  1  0  1  0";
+	for (j=2; j<=nDivDepth + 1; ++j)
+		fout << "\n" << j << "  1  0  0  0";
+
+	fout << "\n";
+// Tractions at the tip
+	if (load != 0)
+	{
+		if (supType == 1)
+		{
+			node = (nDivDepth + 1)*nDivSpan;
+			int nLoad = nDivDepth + 1;
+			fout << "\n\n" << nLoad;
+			double lenth = depth/nDivDepth;
+			for (i=1; i<=nLoad; ++i)
+			{
+				double eq_load;
+				node ++;
+				if (i == 1 ||  i == nLoad) // first or last node on the side
+					eq_load = load*lenth/2.0;
+				else
+					eq_load = load*lenth;
+				fout << "\n" << node << "   " << eq_load << "  0 ";
+			}
+		}
+		else
+		{
+			node = (nDivDepth + 1)*nDivSpan + 1;
+			int nLoad = 2;
+			fout << "\n" << nLoad;
+			fout << "\n" << node   << "   " << load << "  0 ";
+			fout << "\n" << nNodes << "   " << -load << "  0 ";
+		}
+	}
+	else
+		fout << "\n0";
+	fout << "\n\n" << bx << "   " << by;
+}
